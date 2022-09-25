6873ebe8cddc2a47510a3c3204abb676cb7e1af9
Ihor <56516292+degster55@users.noreply.github.com>
2022-09-25 17:30:45 +0300

    Worked with storyboard, added tableView, created CryptoTableViewCell

---
+++ CryptoHelper/CryptoTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CryptoTableViewCell.swift
+//  CryptoHelper
+//
+//  Created by Ігор Ляхович on 25.09.2022.
+//
+
+import UIKit
+
+class CryptoTableViewCell: UITableViewCell {
+
+    static let identifier = "CryptoTableViewCell"
+}
--- CryptoHelper/ViewController.swift
+++ CryptoHelper/ViewController.swift
@@ -7,13 +7,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+     
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: CryptoTableViewCell.identifier, for: indexPath) as? CryptoTableViewCell else {
+            fatalError()
+        }
+        return cell
+    }
+    
 
 }
 
